b5d839dea6c73d9c4311de8cb597fbf68e34bc7e
Andrii Kryvda <kryvdaandrei@gmail.com>
2025-04-08 21:45:03 +0200

    added logo

--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,4 +1,3 @@
-import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
@@ -15,8 +14,8 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <div className="mb-1 flex h-20 w-20 items-center justify-center rounded-md">
+                                <img src="/logo/logo.png" alt="App Logo" className="size-20" />
                             </div>
                             <span className="sr-only">{title}</span>
                         </Link>
